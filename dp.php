96cab8e4819ec32890fcfcc1dfaaedb85ee70888
Liew Yaw Tian <yawtian.liew@qiu.edu.my>
2026-06-13 02:01:36 +0800

    Connect to Railway MySQL

--- dp.php
+++ dp.php
@@ -1,8 +1,11 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "student_registration", 3307);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$conn = mysqli_connect(
+    "MYSQLHOST",
+    "MYSQLUSER",
+    "MYSQLPASSWORD",
+    "MYSQLDATABASE",
+    "MYSQLPORT"
+);
 
 ?>
